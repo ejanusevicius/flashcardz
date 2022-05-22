83ca1b4c0c237ca20ff9a6cc9780667f565ffc22
Edvinas Janusevicius <edvinas.janusevicius@method-analytics.com>
2022-05-22 21:25:39 +0100

    * added .gitignore and appropriate exclusions
    * moved mock data to aws/json

---
+++ aws/lambda/flashcard/get/go.mod
@@ -0,0 +1,5 @@
+module flashcard/flashcard/get
+
+go 1.18
+
+require github.com/aws/aws-lambda-go v1.32.0 // indirect
---
+++ aws/lambda/flashcard/get/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type FlashcardGetEvent struct {}
+type FlashcardGetResponse struct {
+	Message string `json:"message:"`
+}
+
+func LambdaHandler(ctx context.Context, event FlashcardGetEvent) (FlashcardGetResponse, error) {
+	fmt.Println(event)
+	return FlashcardGetResponse{ Message: "Hello world, from Lambda!" }, nil
+}
+
+func main () {
+	lambda.Start(LambdaHandler)
+}
